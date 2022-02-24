43db5c5fb5d584711a974ed6239a9731b44b469a
hnsemage <hnsemage@students.nsbm.ac.lk>
2022-02-24 16:31:02 +0530

    Welcome page

--- app/src/main/java/com/example/choonpaan/MainActivity.kt
+++ app/src/main/java/com/example/choonpaan/MainActivity.kt
@@ -1,11 +1,20 @@
 package com.example.choonpaan
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val login_button: Button =findViewById<Button>(R.id.buttonLogin)
+
+        login_button.setOnClickListener {
+            val intent= Intent(this,Activity_Login::class.java)
+            startActivity(intent)
+        }
     }
 }
