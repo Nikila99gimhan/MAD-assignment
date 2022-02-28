d055134b97e814f8edc5143be4b7bdb3266132d2
hnsemage <hnsemage@students.nsbm.ac.lk>
2022-02-28 15:53:14 +0530

    payment interface

--- app/src/main/java/com/example/choonpaan/Payment_Form.kt
+++ app/src/main/java/com/example/choonpaan/Payment_Form.kt
@@ -3,9 +3,9 @@ package com.example.choonpaan
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 
-class NewNotificationActivity : AppCompatActivity() {
+class Payment_Form : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_new_notification)
+        setContentView(R.layout.activity_payment_form)
     }
 }
