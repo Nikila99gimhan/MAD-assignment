72d87149c421f2c3900c7061170b4fc0eb303bc7
Nikila99gimhan <nikila99fernando.gimhan@gmail.com>
2022-03-06 18:29:15 +0530

    added configs

---
+++ app/src/main/java/com/example/choonpaan/Database/dao/OrderDao.kt
@@ -0,0 +1,19 @@
+package com.example.choonpaan.database.dao
+
+import androidx.room.*
+import com.example.choonpaan.database.entity.Order
+
+@Dao
+interface OrderDao{
+    @Insert(onConflict = OnConflictStrategy.REPLACE)
+    fun insert(order: Order)
+
+    @Query("SELECT * FROM `order` ")
+    fun getAll():List<Order>?
+
+    @Query("SELECT * FROM `order` WHERE id=:id")
+    fun getOrderByID(id:Long):Order?
+
+    @Delete
+    fun delete(order: Order)
+}
