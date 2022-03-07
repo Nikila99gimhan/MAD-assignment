f8cd9012a171c65d845477e4150e3dbf72a41974
sachithradeshan <sachithradeshan567@gmail.com>
2022-03-07 14:58:18 +0530

    Map, Choon Paan Items and Order now layout added

---
+++ app/src/main/java/com/example/choonpaan/MapActivity.kt
@@ -0,0 +1,43 @@
+package com.example.choonpaan
+
+import android.content.Intent
+import android.os.Bundle
+import android.widget.Button
+import com.google.android.material.bottomnavigation.BottomNavigationView
+import androidx.appcompat.app.AppCompatActivity
+import androidx.navigation.findNavController
+import androidx.navigation.ui.AppBarConfiguration
+import androidx.navigation.ui.setupActionBarWithNavController
+import androidx.navigation.ui.setupWithNavController
+import com.example.choonpaan.databinding.ActivityMapBinding
+
+class MapActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMapBinding
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+        val backbutton: Button = findViewById<Button>(R.id.button8)
+
+        backbutton.setOnClickListener {
+            val intent = Intent(this, Activity_Login::class.java)
+            startActivity(intent)
+        }
+        binding = ActivityMapBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        val navView: BottomNavigationView = binding.navView
+
+        val navController = findNavController(R.id.nav_host_fragment_activity_map)
+        // Passing each menu ID as a set of Ids because each
+        // menu should be considered as top level destinations.
+        val appBarConfiguration = AppBarConfiguration(
+            setOf(
+                R.id.navigation_home, R.id.navigation_dashboard, R.id.navigation_notifications
+            )
+        )
+        setupActionBarWithNavController(navController, appBarConfiguration)
+        navView.setupWithNavController(navController)
+    }
+}
