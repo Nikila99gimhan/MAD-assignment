272a2f7d1fb6d69e3f896a2b8df063ef9c9589cc
hnsemage <hnsemage@students.nsbm.ac.lk>
2022-03-09 01:01:52 +0530

    Payment form updated

--- app/src/main/java/com/example/choonpaan/PaymentForm.kt
+++ app/src/main/java/com/example/choonpaan/PaymentForm.kt
@@ -2,11 +2,11 @@ package com.example.choonpaan
 
 import android.content.Intent
 import android.os.Bundle
-import android.widget.Button
 import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
+import com.example.choonpaan.database.entity.Payment
+import com.example.choonpaan.database.repository.PaymentRepo
 import kotlinx.android.synthetic.main.activity_payment_form.*
-import kotlinx.android.synthetic.main.activity_registration.*
 
 class PaymentForm : AppCompatActivity() {
 
@@ -23,6 +23,7 @@ class PaymentForm : AppCompatActivity() {
             val bname=bankNameInput.text.toString()
             val cnumber=cardNumberInput.text.toString()
             val cname=customerNameInput.text.toString()
+            val type=radioGroup2
             val edate=expiryDate.text.toString()
             val cvv=cvvInput.text.toString()
             if(bname == "") {
@@ -38,13 +39,18 @@ class PaymentForm : AppCompatActivity() {
             }else if (!(radioVisa.isChecked) || !(radioMaster.isChecked)){
                 Toast.makeText(this, "Please select the card type!!", Toast.LENGTH_SHORT).show()
             }else{
-                Toast.makeText(this, "Registration Sucessfull", Toast.LENGTH_SHORT).show()
-                val intent = Intent(this, ActivityLogin::class.java)
+                val repo = PaymentRepo.getInstance(this)
+                val payment = Payment(
+                    null,bname,type.toString(),cnumber,cname,edate,cvv,true,false
+                )
+                repo.insert(payment)
+                Toast.makeText(this, "Payment Completed", Toast.LENGTH_SHORT).show()
+                val intent = Intent(this, Payment::class.java)
                 startActivity(intent)
             }
         }
-        val cancel:Button=findViewById(R.id.cancelBtnPay)
-        cancel.setOnClickListener {
+
+        cancelBtnPay.setOnClickListener {
             val intent = Intent(this, ActivityLogin::class.java)
             startActivity(intent)
         }
