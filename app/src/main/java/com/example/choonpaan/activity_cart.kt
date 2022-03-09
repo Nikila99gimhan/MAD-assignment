ea2300b1fd3d8330bf5bbf9edf736cc81d01a8e3
thuthimarian <74374348+thuthimarian@users.noreply.github.com>
2022-03-09 11:01:14 +0530

    Added new arrivals,ebill

---
+++ app/src/main/java/com/example/choonpaan/activity_cart.kt
@@ -0,0 +1,11 @@
+package com.example.choonpaan
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class activity_cart : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_cart)
+    }
+}
